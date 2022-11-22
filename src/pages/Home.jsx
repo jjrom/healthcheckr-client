d6fb3489a894823d98b4f4b32cff8312887f5830
jjrom <jerome.gasperi@gmail.com>
2022-11-22 10:48:02 +0100

    Better layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { For } from "solid-js";
 import { services, setServices } from "../App";
 import ServiceCard from "../components/ServiceCard";
+import { Col, Row } from 'solid-bootstrap';
 
 const refreshServices = async () => {
   const res = await fetch(import.meta.env.VITE_GET_SERVICES_ENDPOINT)
@@ -12,7 +13,12 @@ const Home = () => {
   
   return (
     <div class="container" style="padding-top:50px">
-      <h2 style="padding-bottom:15px">Data services <span class="justify-content-end"><button class="btn btn-primary" onclick={() => refreshServices()}>Refresh</button></span></h2>
+      <h2 style="padding-bottom:15px">
+        <Row>
+          <Col>Data services</Col>
+          <Col class="text-end"><button class="btn btn-primary" onclick={() => refreshServices()}>Refresh</button></Col>
+        </Row>
+      </h2>
       <For each={services()}>
         {(service => <ServiceCard service={service} />)}
       </For>
